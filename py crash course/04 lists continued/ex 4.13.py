0d347e5bef89ea574761064898429ff50ec233f1
Nicolodo <nicolasbernard19@gmail.com>
2025-03-09 16:10:07 +0000

    complete chp4 bar PEP standard reading

---
+++ py crash course/04 lists continued/ex 4.13.py
@@ -0,0 +1,9 @@
+
+buffet = ('ham salads','mint cubes','paddington','plate scrapings','taste teas')
+print(buffet)
+
+# buffet[0] = 'beans'
+
+buffet = buffet[1:] + ('cheese platter',)
+print(buffet)
+
