a4d32b6e561feb25d413f98a5e589fd2c27c0fb4
Nicolodo <nicolasbernard19@gmail.com>
2025-03-25 11:32:00 +0000

    Branches!

--- py crash course/10 Files and Exceptions/exercises/ex11c.py
+++ py crash course/10 Files and Exceptions/exercises/ex11c.py
@@ -1,11 +1,14 @@
 
 import json
 
-filename = 'favourite_number2.json'
+filename = 'favouriteNumber2.json'
 
-try:
-    with open(filename,'w') as f_obj:
-        favNum = json.load(f_obj)
-        print(f"Your favourite number was stored as: {favNum}")
-except FileNotFoundError:
-    print("You have to store your number first!")
+def retrieveFavNum():
+    try:
+        with open(filename) as f_obj:
+            favNum = json.load(f_obj)
+            print(f"Your favourite number was stored as: {favNum}")
+    except FileNotFoundError:
+        print("You have to store your number first!")
+
+retrieveFavNum()
