ab22d650ab9fbb7048d82c31ddef620d61c51b4e
Nicolodo <nicolasbernard19@gmail.com>
2025-03-25 18:20:37 +0000

    Move into function

--- py crash course/10 Files and Exceptions/post exercise 10/greet_user.py
+++ py crash course/10 Files and Exceptions/post exercise 10/greet_user.py
@@ -3,11 +3,13 @@ import json
 
 filename = 'username.json' #get the file we are reading from
 
-try:
-    with open(filename) as f_obj:
-        name = json.load(f_obj)
-except FileNotFoundError:
-    print("please make the file first!")
-else:
-    print(f"Hello {name.capitalize()}!")
+def greet_user():
+    try:
+        with open(filename) as f_obj:
+            name = json.load(f_obj)
+    except FileNotFoundError:
+        print("please make the file first!")
+    else:
+        print(f"Hello {name.capitalize()}!")
 
+greet_user()
