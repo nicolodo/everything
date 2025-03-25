52d1f3bc2851e7f8da2f11d48a1097ec82568cde
Nicolodo <nicolasbernard19@gmail.com>
2025-03-25 18:19:31 +0000

    try except blocks for greet_user.py

--- py crash course/10 Files and Exceptions/post exercise 10/greet_user.py
+++ py crash course/10 Files and Exceptions/post exercise 10/greet_user.py
@@ -3,7 +3,11 @@ import json
 
 filename = 'username.json' #get the file we are reading from
 
-with open(filename) as f_obj:
-    name = json.load(f_obj)
+try:
+    with open(filename) as f_obj:
+        name = json.load(f_obj)
+except FileNotFoundError:
+    print("please make the file first!")
+else:
     print(f"Hello {name.capitalize()}!")
 
