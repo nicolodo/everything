e2c144f2d1ff81256122d3b2dee4cb98b9e53254
Nicolodo <nicolasbernard19@gmail.com>
2025-03-31 09:42:24 +0100

    change var names

---
+++ py crash course/Part 2 Projects/Alien Invasion/settings.py
@@ -0,0 +1,13 @@
+
+class Settings():
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game settings"""
+        self.screen_width = 400
+        self.screen_height = 300
+        self.bg_color = (230,230,230)
+
+
+
+
