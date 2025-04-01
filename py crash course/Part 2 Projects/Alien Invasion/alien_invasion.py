6144cd1de049c97587d1c5213b562ddc3770ef87
Nicolodo <nicolasbernard19@gmail.com>
2025-04-01 16:27:05 +0100

    Add comments to main loop for clarity

--- py crash course/Part 2 Projects/Alien Invasion/alien_invasion.py
+++ py crash course/Part 2 Projects/Alien Invasion/alien_invasion.py
@@ -23,8 +23,10 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
-        ship.update()
+        gf.check_events(ship) # setup ship move flags
+        ship.update() # update ship var values
+
+        # Draw background and ship to screen
         gf.update_screen(ai_settings, screen, ship)
 
 
