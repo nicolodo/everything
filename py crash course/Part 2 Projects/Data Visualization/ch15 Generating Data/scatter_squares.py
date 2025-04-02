d23519d9560eaa4608b5db4e5b78a451afcb0125
Nicolodo <nicolasbernard19@gmail.com>
2025-04-02 10:16:09 +0100

    add and plot values

--- py crash course/Part 2 Projects/Data Visualization/ch15 Generating Data/scatter_squares.py
+++ py crash course/Part 2 Projects/Data Visualization/ch15 Generating Data/scatter_squares.py
@@ -1,8 +1,12 @@
 
 import matplotlib.pyplot as plt
 
+# values
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
 # style output
-plt.scatter(2, 4, s=200) # s is the size of the dots
+plt.scatter(x_values, y_values, s=200) # s is the size of the dots
 plt.title('Square numbers', fontsize=24)
 plt.xlabel('Value', fontsize=14)
 plt.ylabel('Squares of value', fontsize=14)
