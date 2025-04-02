068cd3939ac1791468a0e416a9c7477baea2a4c7
Nicolodo <nicolasbernard19@gmail.com>
2025-04-02 10:30:24 +0100

    Remove edgecolor around each point

--- py crash course/Part 2 Projects/Data Visualization/ch15 Generating Data/scatter_squares.py
+++ py crash course/Part 2 Projects/Data Visualization/ch15 Generating Data/scatter_squares.py
@@ -6,7 +6,8 @@ x_values = list(range(1,1001))
 y_values = [x**2 for x in x_values]
 
 # style output
-plt.scatter(x_values, y_values, s=200) # s is the size of the dots
+plt.scatter(x_values, y_values,edgecolors='none', s=200) # s is dot size 
+    # edgecolor is the color around each point
 plt.title('Square numbers', fontsize=24)
 plt.xlabel('Value', fontsize=14)
 plt.ylabel('Squares of value', fontsize=14)
