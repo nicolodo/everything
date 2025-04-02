a0d1b531e185e906996b74571634d40f533a4f1b
Nicolodo <nicolasbernard19@gmail.com>
2025-04-02 15:13:48 +0100

    Do ex1 part 1 plot first 5 cube numbers

---
+++ py crash course/Part 2 Projects/Data Visualization/ch15 Generating Data/exercises/ex1 Cubes.py
@@ -0,0 +1,18 @@
+
+import matplotlib.pyplot as plt
+
+# setup values
+x_values = [1,2,3,4,5]
+y_values = [1,8,27,64,125]
+
+# setup and style output 
+plt.scatter(x_values,y_values)
+
+# plot onto a window
+plt.show()
+
+
+
+
+
+
