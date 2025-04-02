f743f16f93705b5834f5db9fae75a904bea5c2f0
Nicolodo <nicolasbernard19@gmail.com>
2025-04-02 10:12:05 +0100

    style scatter output

--- py crash course/Part 2 Projects/Data Visualization/ch15 Generating Data/scatter_squares.py
+++ py crash course/Part 2 Projects/Data Visualization/ch15 Generating Data/scatter_squares.py
@@ -1,7 +1,15 @@
 
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4)
+# style output
+plt.scatter(2, 4, s=200) # s is the size of the dots
+plt.title('Square numbers', fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel('Squares of value', fontsize=14)
+
+# Set size of tick labels
+plt.tick_params('both', which='major', labelsize=14)
+
 plt.show()
 
 
