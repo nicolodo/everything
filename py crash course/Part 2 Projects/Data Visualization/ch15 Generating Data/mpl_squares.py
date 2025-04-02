11e860dcb38c97663d87f403e5dab98b22197e55
Nicolodo <nicolasbernard19@gmail.com>
2025-04-02 10:04:15 +0100

    provide x coordinates

--- py crash course/Part 2 Projects/Data Visualization/ch15 Generating Data/mpl_squares.py
+++ py crash course/Part 2 Projects/Data Visualization/ch15 Generating Data/mpl_squares.py
@@ -1,9 +1,10 @@
 
 import matplotlib.pyplot as plt
 
+inputValues = [1,2,3,4,5]
 squares = [1,4,9,16,25]
 # customising the graph
-plt.plot(squares, linewidth=5)
+plt.plot(inputValues, squares, linewidth=5)
 
 # set chart title and label axes
 plt.title('square numbers', fontsize=24)
