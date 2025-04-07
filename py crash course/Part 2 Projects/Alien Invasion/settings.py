9d1903a168b0fb8350a28d724fd62a17fc8c6350
Nicolodo <nicolasbernard19@gmail.com>
2025-04-07 10:54:01 +0100

    Mk setings file for Alien Invasion

---
+++ py crash course/Part 2 Projects/Alien Invasion/settings.py
@@ -0,0 +1,11 @@
+
+class Settings:
+    """A class to store all the settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialise the games settings"""
+        # Screen settings
+        self.SCREEN-SIZE = (400,300)
+        self.BG_COLOR = (200,200,200)
+        
+
