5c947ad99fceb3da0a786c5833074bc49833f61b
Nicolodo <nicolasbernard19@gmail.com>
2025-04-07 10:44:31 +0100

    setup pygame window, event handling todo next

--- py crash course/Part 2 Projects/Alien Invasion/alien_invasion.py
+++ py crash course/Part 2 Projects/Alien Invasion/alien_invasion.py
@@ -1,14 +1,23 @@
 
 # import libraries
-
+import pygame, sys
 
 def run():
-    
-    # initialise pygame
+
+    pygame.init()
+    SIZE = (400, 300)
+    SCREEN = pygame.display.set_mode(SIZE)
+    COLOR = (200,200,200)
+    # choose a name for the window
+    NAME = "Alien Invasion"
+    ACTIVE = True
 
 
     # setup the event loop
-
+    while ACTIVE:
+        for event in pygame.event.get():
+            if event == pygame.QUIT: # close if x clicked
+                sys.exit()
 
 run()
 
