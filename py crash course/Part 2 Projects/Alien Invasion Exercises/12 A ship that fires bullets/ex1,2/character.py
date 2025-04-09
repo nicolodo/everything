3a038501a8c5f5e16af80ac07ed2e26c9651e8d5
Nicolodo <nicolasbernard19@gmail.com>
2025-04-09 16:07:24 +0100

    add screen and screen_rect attribute to character class

--- py crash course/Part 2 Projects/Alien Invasion Exercises/12 A ship that fires bullets/ex1,2/character.py
+++ py crash course/Part 2 Projects/Alien Invasion Exercises/12 A ship that fires bullets/ex1,2/character.py
@@ -7,13 +7,16 @@ class Character():
 
     def __init__(self, screen):
         """Takes in the img shown and the screen used"""
-        path = 'images/sanic2.png'
-        self.image = pygame.image.load(path)
-        self.rect = self.image.get_rect()
         self.screen = screen
 
+        # Load the img and get its rect
+        path = '../images/sanic2.png'
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
         # Start each new ship at the bottom center of the screen.
-        self.rect.centerx = self.screen.centerx
+        self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
     def blitme(self):
