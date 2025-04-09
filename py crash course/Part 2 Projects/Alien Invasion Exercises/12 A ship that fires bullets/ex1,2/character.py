66a92c9a6e00b85c24a503f3685b558f64a49a7f
Nicolodo <nicolasbernard19@gmail.com>
2025-04-09 15:54:54 +0100

    ex2 setup character class

---
+++ py crash course/Part 2 Projects/Alien Invasion Exercises/12 A ship that fires bullets/ex1,2/character.py
@@ -0,0 +1,22 @@
+
+class sanic():
+    """Make a pygame img class which contains sonic"""
+
+    def __init__(self, picture, screen):
+        """Takes in the img shown and the screen used"""
+        path = 'images/sanic2.png'
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect()
+        self.screen = screen
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.centerx = self.screen.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        """Draw the character at its current location"""
+        self.screen.blit(self.image, self.rect)
+
+
+
+
