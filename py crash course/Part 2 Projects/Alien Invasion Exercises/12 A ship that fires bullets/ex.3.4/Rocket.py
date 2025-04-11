355ad6310dda945c23ee8fdeb3de1dcccc598b58
Nicolodo <nicolasbernard19@gmail.com>
2025-04-11 21:00:01 +0100

    import rocket class and do event handling

--- py crash course/Part 2 Projects/Alien Invasion Exercises/12 A ship that fires bullets/ex.3.4/Rocket.py
+++ py crash course/Part 2 Projects/Alien Invasion Exercises/12 A ship that fires bullets/ex.3.4/Rocket.py
@@ -2,6 +2,8 @@
 # move rocket game via keyinput
 
 import sys, pygame
+from SettingsClass import Settings
+from RocketClass import Rocket
 
 def run():
     # setup pygame, screen, name
@@ -11,7 +13,10 @@ def run():
     pygame.init()
     screen = pygame.display.set_mode((400,300))
     pygame.display.set_caption("Rockets of the empire")
-    BLUE = (  0,  0, 128)
+    BLUE = (  0, 128, 128)
+    ai_settings = Settings
+
+    rocket = Rocket(screen, ai_settings)
 
     while True:
         
@@ -19,7 +24,20 @@ def run():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+            # handle keyboard events
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    rocket.rect.top -= 1
+                if event.key == pygame.K_DOWN:
+                    rocket.rect.top += 1
+                if event.key == pygame.K_RIGHT:
+                    rocket.rect.right += 1
+                if event.key == pygame.K_LEFT:
+                    rocket.rect.right -= 1
+
+
         screen.fill(BLUE)
+        rocket.blit_me()
 
         pygame.display.flip()
 
