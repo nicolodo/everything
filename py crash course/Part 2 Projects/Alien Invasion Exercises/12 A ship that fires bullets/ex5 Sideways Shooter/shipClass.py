f5e44edf07b8a765d34e90141bc8d237321f8575
Nicolodo <nicolasbernard19@gmail.com>
2025-04-13 14:03:20 +0100

    setup ship class setup img, pos, and draw func

---
+++ py crash course/Part 2 Projects/Alien Invasion Exercises/12 A ship that fires bullets/ex5 Sideways Shooter/shipClass.py
@@ -0,0 +1,21 @@
+
+import pygame
+
+class Ship:
+    """This is the player's ship"""
+
+    def __init__(self, screen):
+        # store link to the screen passed
+        self.screen = screen
+        self.screenRect = screen.get_rect()
+
+        # setup the ships img
+        pathToImage = 'images/ship.bmp'
+        self.image = pygame.image.load(pathToImage)
+        self.rect = self.image.get_rect()
+        # setup ship position on screen
+        self.rect.left = 0
+        self.rect.centery = self.screenRect.centery
+
+    def blit_me(self):
+        self.screen.blit(self.image, self.rect)
