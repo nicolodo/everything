e05ec02bef466e6c711be76b927670582406f68d
Nicolodo <nicolasbernard19@gmail.com>
2025-04-13 14:15:25 +0100

    add key events that trigger ship movement flags to main loop

--- py crash course/Part 2 Projects/Alien Invasion Exercises/12 A ship that fires bullets/ex5 Sideways Shooter/main.py
+++ py crash course/Part 2 Projects/Alien Invasion Exercises/12 A ship that fires bullets/ex5 Sideways Shooter/main.py
@@ -18,8 +18,25 @@ def run():
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    sys.exit()
+                match event.key:
+                    case pygame.K_q:
+                        sys.exit()
+                    case pygame.K_w:
+                        ship.moveUp = True
+                    case pygame.K_s:
+                        ship.moveDown = True
+                    case _:
+                        continue
+            if event.type == pygame.KEYUP:
+                match event.key:
+                    case pygame.K_w:
+                        ship.moveUp = False
+                    case pygame.K_s:
+                        ship.moveDown = False
+                    case _:
+                        continue
+
+            
 
         # draw to screen
         screen.fill(settings.bgColor)
