da3e23ce79cbf7fc4333f0317bd75812ed88e03c
Nicolodo <nicolasbernard19@gmail.com>
2025-04-18 23:24:22 +0100

    add sprite superclass to bullet

--- py crash course/Part 2 Projects/Alien Invasion Exercises/12 A ship that fires bullets/ex5 Sideways Shooter/bulletClass.py
+++ py crash course/Part 2 Projects/Alien Invasion Exercises/12 A ship that fires bullets/ex5 Sideways Shooter/bulletClass.py
@@ -1,27 +1,33 @@
 
 import pygame
+from pygame.sprite import Sprite
 
-class Bullet:
+class Bullet(Sprite):
     """This models a bullet shot from the rocket"""
 
     def __init__(self, ship, screen):
+        """A class to manage bullets fired from the ship"""
+        # this lets you use Sprite functions I think
+        super(Bullet, self).__init__()
+
         self.screen = screen
         self.screenRect = screen.get_rect()
-        # give it a shape and location
+        # give it a shape and location, color
 
         self.width = 9
         self.height = 3
+        self.color = ( 0, 0, 0)
 
         # shape
-        self.rect = pygame.Rect(0,0,self.width,self.height)
-
+        self.rect = pygame.Rect(0, 0, self.width,self.height)
+        # position at ship
         self.rect.centery = ship.rect.centery
         self.rect.left = ship.rect.right
     
     def update(self):
         self.rect.right += 5
 
-    def blit_me(self, screen):
+    def blit_me(self):
         pygame.draw.rect(self.screen, self.color, self.rect)
         # self.screen.blit(self.image, self.rect)
 
