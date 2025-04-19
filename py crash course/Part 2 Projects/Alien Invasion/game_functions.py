f40bbe667675399ebcc24917f566b53f4f566f5d
Nicolodo <nicolasbernard19@gmail.com>
2025-04-19 16:23:47 +0100

    add create_fleet func in gf

--- py crash course/Part 2 Projects/Alien Invasion/game_functions.py
+++ py crash course/Part 2 Projects/Alien Invasion/game_functions.py
@@ -1,6 +1,7 @@
 
 import sys
 from BulletClass import Bullet
+from alien import Alien
 import pygame
 from settings import Settings
 
@@ -50,7 +51,7 @@ def update_bullets(bullets):
             bullets.remove(bullet)
     # print('bullets: ' + str(len(bullets)))
 
-def update_screen(ai_settings, screen, ship, alien, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     """Update images on the screen and flip to the new screen."""
     # Redraw screen
     screen.fill(ai_settings.COLOR.WHITE)
@@ -58,10 +59,25 @@ def update_screen(ai_settings, screen, ship, alien, bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
 
     # mk the most recently drawn screen visible
     pygame.display.flip()
 
+def create_fleet(ai_settings, screen, aliens):
+    """Create a full fleet of aliens."""
+    # Create an alien and find the number of aliens in a row.
+    # Spacing between each alien is equal to one alien width.
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width -2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
 
+    # Create the first row of aliens.
+    for alien_number in range(number_aliens_x):
+        # Create an alien and place it in the row.
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
 
