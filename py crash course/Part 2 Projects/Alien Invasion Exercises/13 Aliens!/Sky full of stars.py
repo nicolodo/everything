46ac332b2b1bc3b3e27e8088ccfe21bc9728cb2d
Nicolodo <nicolasbernard19@gmail.com>
2025-04-21 10:22:41 +0100

    Plan out the project

--- py crash course/Part 2 Projects/Alien Invasion Exercises/13 Aliens!/Sky full of stars.py
+++ py crash course/Part 2 Projects/Alien Invasion Exercises/13 Aliens!/Sky full of stars.py
@@ -1,3 +1,10 @@
 
+import pygame
 
+# make a window in pygame
+# make a star.bmp, import it to pygame
 
+# using the height and width of screen and star
+# find out how many fit in each row and column
+
+# draw that to the screen and we are done
