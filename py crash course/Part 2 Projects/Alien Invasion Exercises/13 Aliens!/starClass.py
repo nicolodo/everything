26f8fb66996f32f004b06260e117cadd19af371a
Nicolodo <nicolasbernard19@gmail.com>
2025-04-24 18:39:41 +0100

    setup star class

--- py crash course/Part 2 Projects/Alien Invasion Exercises/13 Aliens!/starClass.py
+++ py crash course/Part 2 Projects/Alien Invasion Exercises/13 Aliens!/starClass.py
@@ -1,2 +1,16 @@
 
+import pygame
+
+class star:
+    """This is the star we draw to the screen"""
+
+    def __init__(self, screen):
+        self.screen = screen
+        self.screenRect = screen.get_rect()
+
+        self.image = pygame.image.load('images/star.bmp')
+        self.rect = self.image.get_rect()
+
+    def blit_me():
+        self.screen.blit(self.image, self.screenRect)
 
