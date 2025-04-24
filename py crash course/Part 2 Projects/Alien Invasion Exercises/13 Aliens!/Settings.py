d8595f5e1ed1699d9e658b163efdfe15c47616c2
Nicolodo <nicolasbernard19@gmail.com>
2025-04-24 18:27:10 +0100

    setup settings file

---
+++ py crash course/Part 2 Projects/Alien Invasion Exercises/13 Aliens!/Settings.py
@@ -0,0 +1,10 @@
+
+class Settings:
+    """Settings for the stars scene in pygame"""
+    
+    def __init__(self):
+        self.bgColor = (200,200, 0)
+        self.screenWidth = 400
+        self.screenHeight = 300
+        self.screenSize = (self.screenWidth,self.screenHeight)
+
