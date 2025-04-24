a7561429d7f0dc60a07a52e48e68c411960b9657
Nicolodo <nicolasbernard19@gmail.com>
2025-04-24 18:28:01 +0100

    import settings & setup event handling loop

--- py crash course/Part 2 Projects/Alien Invasion Exercises/13 Aliens!/Sky full of stars.py
+++ py crash course/Part 2 Projects/Alien Invasion Exercises/13 Aliens!/Sky full of stars.py
@@ -1,15 +1,26 @@
 
+import sys
 import pygame
-import settings
+import Settings
 
 # make a window in pygame
 def run():
     pygame.init()
-    screen = pygame.display.set_mode((settings.screen_size))
+    settings = Settings.Settings()
+    screen = pygame.display.set_mode((settings.screenSize))
     pygame.display.set_caption("Wake up Super Star!")
 
     while True:
-       continue 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event == pygame.K_q:
+                    sys.exit()
+
+        screen.fill(settings.bgColor)
+
+        pygame.display.flip()
 run()
 # make a star.bmp, import it to pygame
 
