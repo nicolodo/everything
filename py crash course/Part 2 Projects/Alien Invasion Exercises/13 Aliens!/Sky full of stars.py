18070f675189ca5eda849797d76a32eb9e6a3575
Nicolodo <nicolasbernard19@gmail.com>
2025-04-24 18:15:53 +0100

    c13 ex1 make stub file now filling it out

--- py crash course/Part 2 Projects/Alien Invasion Exercises/13 Aliens!/Sky full of stars.py
+++ py crash course/Part 2 Projects/Alien Invasion Exercises/13 Aliens!/Sky full of stars.py
@@ -1,7 +1,16 @@
 
 import pygame
+import settings
 
 # make a window in pygame
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((settings.screen_size))
+    pygame.display.set_caption("Wake up Super Star!")
+
+    while True:
+       continue 
+run()
 # make a star.bmp, import it to pygame
 
 # using the height and width of screen and star
