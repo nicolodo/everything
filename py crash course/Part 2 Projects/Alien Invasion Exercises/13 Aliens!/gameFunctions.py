4b462500ff606fa3b332b51709df6432c8eee7de
Nicolodo <nicolasbernard19@gmail.com>
2025-04-25 16:04:27 +0100

    add update star function location func to gf

--- py crash course/Part 2 Projects/Alien Invasion Exercises/13 Aliens!/gameFunctions.py
+++ py crash course/Part 2 Projects/Alien Invasion Exercises/13 Aliens!/gameFunctions.py
@@ -33,11 +33,26 @@ def makeStars(screen, stars, wobbliness):
             newStar.rect.y = 2*y*star.height + star.height/2 + randint(0, wobbliness)
             newStar.rect.x = 2*x*star.width + star.width/2 + randint(0, wobbliness)
             stars.add(newStar)
-    stars.draw(screen)
+
+def updateStarLocation(screen, stars, wobbliness):
+    star = Star(screen)
+    # horizontal line of stars placement
+    starsPerWidth = int(settings.screenWidth/star.width)
+    starsPerHeight = int(settings.screenHeight/star.height)
+    for currentStar in stars.copy():
+        star0 = currentStar
+        for x in range(int(starsPerWidth/2)):
+            for y in range(int(starsPerHeight/2)):
+                # vertical line of stars 
+                currentStar.rect.y = 2*y*star.height + star.height/2 + randint(0, wobbliness)
+                currentStar.rect.x = 2*x*star.width + star.width/2 + randint(0, wobbliness)
+        stars.remove(star0)
+        stars.add(currentStar)
 
 def updateScreen(screen, stars, wobbliness):
     screen.fill(settings.bgColor)
-    makeStars(screen, stars, wobbliness)
+    updateStarLocation(screen, stars, wobbliness)
+    stars.draw(screen)
     # star.blit_me()
 
     pygame.display.flip()
